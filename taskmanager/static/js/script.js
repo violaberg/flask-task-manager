ae20960769d3c3d8612ef3732ac23ad8a2fb407c
violaberg <>
2024-01-17 13:15:16 +0000

    Add task manipulation

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker init
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select init
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
