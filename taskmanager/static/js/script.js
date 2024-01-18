d2b5647149db5aef1bf674bc8b4c2a4227245ab2
violaberg <>
2024-01-18 13:13:04 +0000

    Add more tasks, fix error

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,19 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let sidenav = document.querySelectorAll('.sidenav');
+document.addEventListener("DOMContentLoaded", function () {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 
-    // datepicker init
-    let datepicker = document.querySelectorAll('.datepicker');
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
     M.Datepicker.init(datepicker, {
-        format: "dd mmm, yyyy",
-        i18n: {done: "Select"}
+        format: "dd mmmm, yyyy",
+        i18n: { done: "Select" }
     });
 
-    // select init
-    let selects = document.querySelectorAll('select');
+    // select initialization
+    let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
 
-    //collapsible init
+    // collapsible initializataion
     let collapsibles = document.querySelectorAll(".collapsible");
     M.Collapsible.init(collapsibles);
 });
